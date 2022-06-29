f14bbfd6e58c9a523d5901ec2a6515cadb510961
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-06-29 09:00:09 -0300

    formating

--- react advanced master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react advanced master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,47 +1,50 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 const MultipleReturns = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState('default user');
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('default user')
 
-  useEffect (() =>{
+  useEffect(() => {
     fetch(url)
-    .then((resp) => {
-      if (resp.status >= 200 && resp.status <= 299){
-        return resp.json()
-      }
-      else{
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json()
+        } else {
+          setIsLoading(false)
+          setIsError(true)
+          throw new Error(resp.statusText)
+        }
+      })
+      .then((user) => {
+        const { login } = user
+        setUser(login)
         setIsLoading(false)
-        setIsError(true)
-        throw new Error(resp.statusText)
-      }
-    })
-    .then ((user) => {
-      const {login} = user;
-      setUser(login);
-      setIsLoading(false);
-    })
-    .catch ((error) => console.log(error))
-  },[])
+      })
+      .catch((error) => console.log(error))
+  }, [])
 
   if (isLoading) {
-    return <div>
-      <h1>loading...</h1>
-    </div>
+    return (
+      <div>
+        <h1>loading...</h1>
+      </div>
+    )
   }
-  
+
   if (isError) {
-    return  <>
-      <h1>Error...</h1>
-    </>
+    return (
+      <>
+        <h1>Error...</h1>
+      </>
+    )
   }
 
   return (
-  <div>
-    <h1>{user}</h1>
-  </div>
-  );
-};
+    <div>
+      <h1>{user}</h1>
+    </div>
+  )
+}
 
-export default MultipleReturns;
+export default MultipleReturns
