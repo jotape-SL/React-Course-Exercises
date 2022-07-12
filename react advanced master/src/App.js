5fc584d7ffb86e61cdbe1c4f28340275cad22e1f
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-12 04:57:22 -0300

    refactoring of the old function to suit the multiple returns

--- react advanced master/src/App.js
+++ react advanced master/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Seput from './tutorial/4-forms/setup/1-controlled-inputs'
+import React from 'react';
+import Seput from './tutorial/4-forms/setup/2-multiple-inputs';
 
 function App() {
   return (
     <div className='container'>
       <Seput />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
