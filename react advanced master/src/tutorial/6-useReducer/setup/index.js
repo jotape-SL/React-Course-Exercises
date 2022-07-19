73ee2c5487cd3dbb1f7ddd091e99d6badf72a5d1
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-19 13:16:47 -0300

    reducer refactor

--- react advanced master/src/tutorial/6-useReducer/setup/index.js
+++ react advanced master/src/tutorial/6-useReducer/setup/index.js
@@ -2,9 +2,8 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
-const reducer = (state, actin) => {
-  //comment
-};
+import { reducer } from './reducer';
+
 const defaultState = {
   people: [],
   isModalOpen: false,
@@ -13,15 +12,27 @@ const defaultState = {
 const Index = () => {
   const [name, setName] = useState('');
   const [state, dispatch] = useReducer(reducer, defaultState);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: 'ADD_ITEM', payload: newItem });
+      setName('');
     } else {
+      dispatch({ type: 'NO_VALUE' });
     }
   };
+
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE_MODAL' });
+  };
+
   return (
     <>
-      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
       <form onSubmit={handleSubmit} className='form'>
         <div>
           <input
@@ -34,8 +45,15 @@ const Index = () => {
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className='item  '>
             <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: 'REMOVE_ITEM', payload: person.id })
+              }
+            >
+              remove
+            </button>
           </div>
         );
       })}
